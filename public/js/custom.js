9073c115bcbdd712818988cf9cf2d13b01b3745d
Asya Toteva <asya@rainbowgrp.co.uk>
2020-07-01 06:56:27 +0300

    - Add confirm action(open or edit) on button with previously set hyperlink

--- public/js/custom.js
+++ public/js/custom.js
@@ -14,6 +14,19 @@ $(document).ready(function () {
     $('.delete-form').on('click', function () {
         return window.confirm('Are you sure you want to delete ?');
     });
+    
+    /* Confirm the action on click */
+    $('.action-url').on('click', function(e){
+        e.preventDefault();
+        let currBtn = e.currentTarget;
+        let url = currBtn.href;
+        let id = $(currBtn).data('id');
+        if (confirm(`Are you sure to go to: ${url} ?`)) {
+           window.open(url);
+        } else {
+            window.location.href = `/button/${id}/edit`;
+       }
+    })
 });
 
 
--- resources/views/button/index.blade.php
+++ resources/views/button/index.blade.php
@@ -8,18 +8,23 @@
             @php 
                 $url= $button->link;
                 $target= '_blank';
+                $action = 'action-url';
+                $id = $button->id;
             @endphp
         @else
              @php 
                 $url = route('button.edit', $button->id);
                 $target= '_self';
+                $action = '';
+                $id= '';
              @endphp
         @endif 
         
         <div class="col-md-4 col-sm-4 col-xs-6">
-            <a class="btn btn-lg btn-block"
+            <a class="btn btn-lg btn-block {{ $action }}"
                style="background-color:{{ $button->color }}"
-               href="{{ $url }}" target="{{ $target}}"          
+               href="{{ $url }}" target="{{ $target}}"
+               data-id="{{ $id }}"
             >
                 <i class="icon-plus-sign"></i>
             </a>
